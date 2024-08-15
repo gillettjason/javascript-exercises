a6019acb072e69c8b5ad6d6fb1c716ee82fb9869
gillettjason <jase.gillett@gmail.com>
2024-08-15 07:55:54 +0100

    Calc exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, next) => {
+    return total + next;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, next) => {
+    return total * next;
+  });
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = n => n <= 1 ? 1 : n * factorial(n - 1);
+
 
 // Do not edit below this line
 module.exports = {
